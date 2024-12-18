1037d7498c8b6cb0bdfd9b145fff0af33f06dd92
hasnaeorj <hasnaeourhejji2@gmail.com>
2024-12-18 14:48:46 +0000

    hm

---
+++ homework1/part2.py
@@ -0,0 +1,9 @@
+
+celsius = float(input("Enter the temperature in Celsius: "))
+    
+    
+fahrenheit = (celsius * 9/5) + 32
+    
+  
+print(f"{celsius}° Celsius is equal to {fahrenheit}° Fahrenheit.")
+
---
+++ homework1/part3.py
@@ -0,0 +1,14 @@
+
+user_input = input("Enter a number: ")
+    
+number_int = int(user_input)   # Convert to integer
+    
+number_float = float(user_input)  # Convert to float
+        
+number_str = str(user_input)     # Convert to string
+   
+
+print(f"Integer: {number_int}, Type: {type(number_int)}")
+print(f"Float: {number_float}, Type: {type(number_float)}")
+print(f"String: {number_str}, Type: {type(number_str)}")
+
